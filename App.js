e0690e2a97264a0b3d3d09cfffa1010119dca400
Guin <vohyyy321@gmail.com>
2025-02-15 09:51:21 +0700

    update

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import MenuScreen from './components/MenuScreen';
 import HomeScreen from './components/HomeScreen';
 
@@ -14,9 +15,8 @@ export default function App() {
 }
 
 const MyStack = () => {
-  const Stack = createStackNavigator()
+  const Stack = createStackNavigator();
   return (
-
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Menu" component={MenuScreen} />
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import MenuScreen from './MenuScreen';
 
 export default function HomeScreen() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+        // style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        <View>
+            <Text onPress={() => navigation.navigate(Menu)}>Menu</Text>
         </View>
     );
 }
